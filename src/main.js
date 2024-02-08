008c60b5d9ed87aef8c7d694abbdb3430e059bed
Muxammadbobur <bobur0172@gmail.com>
2024-02-08 17:57:43 +0500

    fix build error

--- src/main.js
+++ src/main.js
@@ -1,11 +1,4 @@
 import "./assets/main.css";
-//
-import "./vendor/jquery/jquery.min.js";
-
-import "./assets/js/custom.js";
-import "./assets/js/owl.js";
-import "./assets/js/slick.js";
-import "./assets/js/accordions.js";
 
 import { createApp } from "vue";
 import App from "./App.vue";
